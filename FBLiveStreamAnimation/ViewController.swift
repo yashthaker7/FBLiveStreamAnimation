4bf695e57dada64e3461bd63db706079c8e84a12
Yash Thaker <yash@yashs-imac.local>
2018-05-18 21:03:22 +0530

    Initial Commit

---
+++ FBLiveStreamAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FBLiveStreamAnimation
+//
+//  Created by Yash Thaker on 18/05/18.
+//  Copyright © 2018 Yash Thaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
